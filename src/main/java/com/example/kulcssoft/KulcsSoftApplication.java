2f1182b56719bb7dc1bde15de178ce44b9d607e7
Csonti <csontostamas1991@gmail.com>
2019-02-19 11:38:21 +0100

    set up intellij

--- src/main/java/com/example/kulcssoft/KulcsSoftApplication.java
+++ src/main/java/com/example/kulcssoft/KulcsSoftApplication.java
@@ -1,14 +1,13 @@
-package com.example.practice;
+package com.example.kulcssoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PracticeApplication {
+public class KulcsSoftApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PracticeApplication.class, args);
+		SpringApplication.run(KulcsSoftApplication.class, args);
 	}
 
 }
-
